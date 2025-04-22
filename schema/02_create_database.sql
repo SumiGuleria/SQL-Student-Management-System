80e68a29b081138cfdf212f6eb1d600e2924dc96
Your Name <sushmitguleria@gmail.com>
2025-04-22 18:21:40 -0400

    adding the grades table

---
+++ schema/02_create_database.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE Grades (
+    grade_id INT PRIMARY KEY IDENTITY,
+    student_id INT,
+    course_id INT,
+    grade CHAR(1),
+    FOREIGN KEY (student_id) REFERENCES Students(student_id),
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+);
